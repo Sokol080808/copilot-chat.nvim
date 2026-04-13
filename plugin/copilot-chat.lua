87629d319a7b99d986c0b8cd75b99da133d97d5c
Aleksey Sokolnikov <assokolnikov@gmail.com>
2026-04-14 01:49:48 +0300

    feat: show usage limits in chat header

--- plugin/copilot-chat.lua
+++ plugin/copilot-chat.lua
@@ -15,3 +15,7 @@ end, {})
 vim.api.nvim_create_user_command("CopilotChatAsk", function()
   require("copilot-chat").ask()
 end, {})
+
+vim.api.nvim_create_user_command("CopilotChatUsage", function()
+  require("copilot-chat").usage()
+end, {})
